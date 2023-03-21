7354b88faecbd86748e2da1bf96e980094dbde65
nam-do-sam <nrsn2may@gmail.com>
2023-03-20 19:02:08 -0500

    nabvar version 3

--- js/app.js
+++ js/app.js
@@ -1,3 +1,61 @@
+// hidden and show with scroll bg header
+
+let header = document.getElementById('header');
+let sale = document.getElementById('sale');
+
+// scroll var 
+const scroll_down = document.getElementById('scroll-down');
+
+window.addEventListener( 'scroll' , function (params) {
+    // if (prevY > window.scrollY){
+    //     console.log('subiendo');
+    //     // sale.classList.remove('hidden');
+    //     // solo aplica bg-black en tamaños - md ipad
+    //     // header.classList.remove('md:bg-black'); 
+    // }else{
+    //     console.log('bajando')
+    //     sale.classList.add('hidden');
+    //     header.classList.add('md:bg-black')
+    // }
+
+    // si scroll < 100 - mostrar sale
+    if (window.scrollY < 100) {
+        sale.classList.remove('hidden');
+        header.classList.remove('md:bg-white'); 
+    }
+    else{
+        sale.classList.add('hidden');
+        header.classList.add('md:bg-white');
+    }
+
+    // show arrow show scroll
+    // if (window.scrollY > 1000) {
+    //     scroll_down.classList.remove('hidden');
+    // }
+    // else{
+    //     scroll_down.classList.add('hidden');
+    // }
+})
+
+// button to open and close nav menu
+const btn_menu = document.getElementById('btn-menu');
+let open = document.getElementById('open');
+let close = document.getElementById('close');
+
+const menu_nav = document.getElementById('menu-nav');
+
+btn_menu.addEventListener('click', function (params) {
+    open.classList.toggle('hidden');
+    menu_nav.classList.toggle('hidden');
+
+    close.classList.toggle('hidden');
+    menu_nav.classList.toggle('block');
+})
+
+
+
+
+
 // portada principal
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 50,
