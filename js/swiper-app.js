5f8c861eeb5f87f978d6a4ff207a87e4ba19ba0e
nam-do-sam <nrsn2may@gmail.com>
2023-04-08 13:19:01 -0500

    sabado 08

--- js/swiper-app.js
+++ js/swiper-app.js
@@ -72,3 +72,36 @@ var swiper = new Swiper(".mySwiper2", {
         clickable: true,
     },
 });
+
+
+/////////////////////////////////////////////
+// Tratamientos populares
+/////////////////////////////////////////////
+
+var swiper = new Swiper(".swiper-3", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+        },
+    },
+});
