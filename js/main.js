270312e81aef277c5ec869517847160124ecff96
nam-do-sam <nrsn2may@gmail.com>
2023-04-24 19:07:20 -0500

    agregando seo seccion faciales

--- js/main.js
+++ js/main.js
@@ -4,27 +4,59 @@
 ////////////////////////////////////////
 // navbar hidden 
 ////////////////////////////////////////
+let header = document.getElementById('header');
+let sale = document.getElementById('sale');
 
+// scroll var 
+const scroll_down = document.getElementById('scroll-down');
 
+window.addEventListener('scroll', function (params) {
+    // if (prevY > window.scrollY){
+    //     console.log('subiendo');
+    //     // sale.classList.remove('hidden');
+    //     // solo aplica bg-black en tamaños - md ipad
+    //     // header.classList.remove('md:bg-black'); 
+    // }else{
+    //     console.log('bajando')
+    //     sale.classList.add('hidden');
+    //     header.classList.add('md:bg-black')
+    // }
 
+    // si scroll < 100 - mostrar sale
+    if (window.scrollY < 100) {
+        sale.classList.remove('hidden');
+        header.classList.remove('md:bg-white');
+    }
+    else {
+        sale.classList.add('hidden');
+        header.classList.add('md:bg-white');
+    }
 
+    // show arrow show scroll
+    // if (window.scrollY > 1000) {
+    //     scroll_down.classList.remove('hidden');
+    // }
+    // else{
+    //     scroll_down.classList.add('hidden');
+    // }
+})
 
 ////////////////////////////////////////
-// scroll hidden
+// BUTTON TO OPEN AND CLOSE NAV MENU
 ////////////////////////////////////////
+const btn_menu = document.getElementById('btn-menu');
+let open = document.getElementById('open');
+let close = document.getElementById('close');
 
+const menu_nav = document.getElementById('menu-nav');
 
+btn_menu.addEventListener('click', function (params) {
+    open.classList.toggle('hidden');
+    menu_nav.classList.toggle('hidden');
 
-
-
-
-
-
-////////////////////////////////////////
-// slider after and before
-////////////////////////////////////////
-
-
+    close.classList.toggle('hidden');
+    menu_nav.classList.toggle('block');
+})
 
 
 
