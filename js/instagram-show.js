438109fc5f22e3ff03e337a7b8e294b37f82b014
nam-do-sam <nrsn2may@gmail.com>
2023-05-20 13:45:58 -0500

    agregue seccion de zonas de depilacion laser

--- js/instagram-show.js
+++ js/instagram-show.js
@@ -14,3 +14,22 @@ btnMostrarMas.addEventListener('click', function() {
     btnMostrarMas.style.display = 'none';
   }
 });
+
+
+
+
+
+
+// tab windonw
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+}
---
+++ js/vanilla.js
@@ -0,0 +1,33 @@
+// // tab windonw
+// function openTab(evt, tabName) {
+//     var i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+//     document.getElementById(tabName).style.display = "block";
+// }
+
+
+
+function openTab(event, tabName) {
+    var i, tabContent, tab;
+  
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+  
+    tab = document.getElementsByClassName("tab");
+    for (i = 0; i < tab.length; i++) {
+      tab[i].className = tab[i].className.replace("active-tab", "");
+    }
+  
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active-tab";
+  }
+  
